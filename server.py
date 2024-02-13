967ca0f86565a7051d903f389add329592478633
Connor Holm <connorjholm@gmail.com>
2024-02-13 10:40:03 -0600

    base page routes

--- server.py
+++ server.py
@@ -1,7 +1,19 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, url_for
 
 app = Flask(__name__)
 
 @app.route('/')
 def render_index():
     return render_template('index.html')
+
+@app.route('/create_event')
+def render_create_event():
+    return render_template('create_event.html')
+
+@app.route('/event/<int:event_id>')
+def render_event(event_id):
+    return render_template('event.html', event_id=event_id)
+
+@app.route('/profile')
+def render_profile():
+    return render_template('profile.html')
