e0033197745c421f9d61a9379868f1a38f27f5e2
J-Huang01 <huan2460@umn.edu>
2024-02-13 22:14:07 -0600

    add basic auth logic

--- server.py
+++ server.py
@@ -6,6 +6,8 @@ from authlib.integrations.flask_client import OAuth
 import database
 
 app = Flask(__name__)
+app.secret_key = env.get("APP_SECRET_KEY")
+
 with app.app_context():
     database.setup()
 
@@ -13,6 +15,17 @@ ENV_FILE = find_dotenv()
 if ENV_FILE:
     load_dotenv(ENV_FILE)
 
+oauth = OAuth(app)
+
+oauth.register(
+    "auth0",
+    client_id=env.get("AUTH0_CLIENT_ID"),
+    client_secret=env.get("AUTH0_CLIENT_SECRET"),
+    client_kwargs={
+        "scope": "openid profile email",
+    },
+    server_metadata_url=f'https://{env.get("AUTH0_DOMAIN")}/.well-known/openid-configuration'
+)
 
 
 @app.route('/')
@@ -77,3 +90,30 @@ def api_event(event_id):
 @app.route('/api/users/<user_id>/events')
 def api_user_events(user_id):
     return database.get_user_events(user_id)
+
+@app.route("/login")
+def login():
+    return oauth.auth0.authorize_redirect(
+        redirect_uri=url_for("callback", _external=True)
+    )
+
+@app.route("/callback", methods=["GET", "POST"])
+def callback():
+    token = oauth.auth0.authorize_access_token()
+    session["user"] = token
+    return redirect("/")
+
+@app.route("/logout")
+def logout():
+    session.clear()
+    return redirect(
+        "https://" + env.get("AUTH0_DOMAIN")
+        + "/v2/logout?"
+        + urlencode(
+            {
+                "returnTo": url_for("index", _external=True),
+                "client_id": env.get("AUTH0_CLIENT_ID"),
+            },
+            quote_via=quote_plus,
+        )
+    )
