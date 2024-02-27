e600b5b016dd32d8384186e3bb72cf80e2f5a401
Hads23 <kotif@umn.edu>
2024-02-27 13:04:03 -0600

    Users must have a start time that begins before end time

--- static/create_event.js
+++ static/create_event.js
@@ -2,3 +2,15 @@ window.onload = function() {
     let input = document.getElementById("stacked-event-location");
     let autocomplete = new google.maps.places.Autocomplete(input);
 }
+
+function validateTimes() {
+    var start = document.getElementById("stacked-event-time").value;
+    var end = document.getElementById("stacked-event-end").value;
+
+    if (start >= end) {
+        alert("End time must be greater than start time.");
+        return false;
+    }
+
+    return true;
+}
